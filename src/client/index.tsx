2114e1fce151452d7f71de6824e8bf57172fbae2
ruslan kosh <koshmukhan@gmail.com>
2024-07-17 16:35:25 +0500

    add eslint prettier

--- src/client/index.tsx
+++ src/client/index.tsx
@@ -1,19 +1,13 @@
-import React from 'react';
-import {createRoot} from "react-dom/client";
-import { App } from './App'
+import React from "react";
+import { createRoot } from "react-dom/client";
+import { App } from "./App";
 
-
-const rootEl = document.getElementById('root');
-if (!rootEl) throw new Error('Root element with id root is missing');
+const rootEl = document.getElementById("root");
+if (!rootEl) throw new Error("Root element with id root is missing");
 const root = createRoot(rootEl);
 
 root.render(
-    <React.StrictMode>
-        <App></App>
-    </React.StrictMode>
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
 );
-
-
-
-
-
